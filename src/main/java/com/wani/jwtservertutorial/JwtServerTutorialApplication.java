20f83a622c75356baf52d33f170dd3db37ba338e
wani <oen142@naver.com>
2020-12-18 16:25:17 +0900

    Initial commit

---
+++ src/main/java/com/wani/jwtservertutorial/JwtServerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.jwtservertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtServerTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtServerTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/jwtservertutorial/JwtServerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.jwtservertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtServerTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
